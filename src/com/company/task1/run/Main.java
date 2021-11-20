7e23f11823b6bd9a87bf9ed2b43328fadffae8b1
Nastya Ber <anastasiaberz212@gmail.com>
2021-11-20 08:16:50 +0300

    task 1

---
+++ src/com/company/task1/run/Main.java
@@ -0,0 +1,8 @@
+package com.company.task1.run;
+import com.company.task1.util.Task1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Task1.solution(1,2));
+    }
+}
